fe3ca15885989bfa006d9053cca5b20d9cd1b9b8
Suman Kisku <sumankisku1@gmail>
2023-08-10 14:17:48 +0530

    Removed unused button, fix NaN bug

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -10,102 +10,107 @@ let allProducts = [...store];
 
 // save store in the localStorage
 const saveCartInLS = (cart) => {
-    localStorage.setItem("cart", JSON.stringify(cart));
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case "addToCart":
-            // eslint-disable-next-line no-case-declarations
-            let itemToAdd = {...allProducts.find((prod) => prod.id === action.id)};
+  switch (action.type) {
+    case "addToCart":
+      // eslint-disable-next-line no-case-declarations
+      let itemToAdd = allProducts.find((prod) => prod.id === action.id);
+      if (!itemToAdd) {
+        // If the product is not found, return the current state without any changes.
+        saveCartInLS(state);
+        return state;
+      }
 
-            // eslint-disable-next-line no-case-declarations
-            let itemAlreadyInCart = state.find((prod) => prod.id === action.id)
+      // eslint-disable-next-line no-case-declarations
+      let itemAlreadyInCart = state.find((prod) => prod.id === action.id)
+      
+      if (itemAlreadyInCart) {
+        console.log(typeof itemAlreadyInCart["qnt"]);
+        itemAlreadyInCart["qnt"] = itemToAdd["qnt"] + 1;
+        saveCartInLS([...state]);
+        return [...state];
+      } else {
+        // Initialize qnt with 1
+        itemToAdd["qnt"] = 1;
+      }
+      // Save and Return a new state with the itemToAdd added to the cart array.
+      saveCartInLS([...state, itemToAdd]);
+      return [...state, itemToAdd];
+    case "deleteFromCart":
+      state.forEach((prod) => {
+        if (prod.id == action.id) {
+          prod.qnt = 0;
+        }
+      })
+      // eslint-disable-next-line no-case-declarations
+      let afterDelete = state.filter((prod) => {
+        return prod.id != action.id;
+      })
+      toast.error("Item removed from cart", {
+        style: {
+          border: '1px solid #1976d2',
+          padding: '16px',
+          color: '#1976d2',
+        },
+        iconTheme: {
+          primary: 'red',
+          secondary: '#FFFAEE',
+        },
+      });
+      saveCartInLS([...afterDelete]);
+      return [...afterDelete];
+    case "increaseqnt":
+      let itemNeedToIncreaseqnt = state.find((prod) => prod.id === action.id);
+      itemNeedToIncreaseqnt["qnt"] += 1;
+      saveCartInLS([...state]);
+      return [...state];
+    case "decreaseqnt":
+      let itemNeedToDecreaseqnt = state.find((prod) => prod.id === action.id);
+      if (itemNeedToDecreaseqnt["qnt"] > 0) {
+        itemNeedToDecreaseqnt["qnt"] = itemNeedToDecreaseqnt["qnt"] - 1;
+      }
 
-            if (itemAlreadyInCart) {
-                // if item alreay in cart just increase the qnt
-                itemAlreadyInCart["qnt"] = Number(itemToAdd["qnt"] + 1); // for some weired reason, sometimes there was NaN in quantity.
-                saveCartInLS([...state]);
-                return [...state];
-            } else {
-                // Initialize qnt with 1
-                itemToAdd["qnt"] = 1;
-            }
-            // Save and Return a new state with the itemToAdd added to the cart array.
-            saveCartInLS([...state, itemToAdd]);
-            return [...state, itemToAdd];
-        case "deleteFromCart":
-            state.forEach((prod) => {
-                if (prod.id == action.id) {
-                    prod.qnt = 0;
-                }
-            })
-            // eslint-disable-next-line no-case-declarations
-            let afterDelete = state.filter((prod) => {
-                return prod.id != action.id;
-            })
-            toast.error("Item removed from cart", {
-                style: {
-                    border: '1px solid #1976d2',
-                    padding: '16px',
-                    color: '#1976d2',
-                },
-                iconTheme: {
-                    primary: 'red',
-                    secondary: '#FFFAEE',
-                },
-            });
-            saveCartInLS([...afterDelete]);
-            return [...afterDelete];
-        case "increaseqnt":
-            let itemNeedToIncreaseqnt = state.find((prod) => prod.id === action.id);
-            itemNeedToIncreaseqnt["qnt"] += 1;
-            saveCartInLS([...state]);
-            return [...state];
-        case "decreaseqnt":
-            let itemNeedToDecreaseqnt = state.find((prod) => prod.id === action.id);
-            if (itemNeedToDecreaseqnt["qnt"] > 0) {
-                itemNeedToDecreaseqnt["qnt"] = itemNeedToDecreaseqnt["qnt"] - 1;
-            }
-
-            if (itemNeedToDecreaseqnt["qnt"] === 0) {
-                // eslint-disable-next-line no-case-declarations
-                let afterDelete = state.filter((prod) => {
-                    return prod.id != action.id;
-                })
-                toast.error("Item removed from cart", {
-                    style: {
-                        border: '1px solid #1976d2',
-                        padding: '16px',
-                        color: '#1976d2',
-                    },
-                    iconTheme: {
-                        primary: 'red',
-                        secondary: '#FFFAEE',
-                    },
-                });
-                saveCartInLS([...afterDelete]);
-                return [...afterDelete];
-            }
-            saveCartInLS([...state]);
-            return [...state];
-        case "clearCart":
-            return [];
-        default:
-            saveCartInLS([...state]);
-            return state;
-    }
+      if (itemNeedToDecreaseqnt["qnt"] === 0) {
+        // eslint-disable-next-line no-case-declarations
+        let afterDelete = state.filter((prod) => {
+          return prod.id != action.id;
+        })
+        toast.error("Item removed from cart", {
+          style: {
+            border: '1px solid #1976d2',
+            padding: '16px',
+            color: '#1976d2',
+          },
+          iconTheme: {
+            primary: 'red',
+            secondary: '#FFFAEE',
+          },
+        });
+        saveCartInLS([...afterDelete]);
+        return [...afterDelete];
+      }
+      saveCartInLS([...state]);
+      return [...state];
+    case "clearCart":
+      return [];
+    default:
+      saveCartInLS([...state]);
+      return state;
+  }
 }
 
 let initialCartState = [];
 if (localStorage.getItem("cart")) {
-    initialCartState = JSON.parse(localStorage.getItem("cart"));
+  initialCartState = JSON.parse(localStorage.getItem("cart"));
 }
 
 export const CartContextProvider = ({ children }) => {
-    const [cart, dispatch] = useReducer(reducer, initialCartState);
+  const [cart, dispatch] = useReducer(reducer, initialCartState);
 
-    return <CartContext.Provider value={{ cart, dispatch }}>
-        {children}
-    </CartContext.Provider>
+  return <CartContext.Provider value={{ cart, dispatch }}>
+    {children}
+  </CartContext.Provider>
 }
