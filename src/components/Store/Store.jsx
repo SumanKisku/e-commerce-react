e8fa2f92f5eb13e5d71d540aae1ce198e1dca703
Suman Kisku <sumankisku1@gmail>
2023-08-17 19:55:04 +0530

    add filter feat

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,20 +1,46 @@
+import { useState, useEffect } from "react";
 import { Grid } from "@mui/material";
-import { store } from '../../../store'
+import { store } from "../../../store";
 import ProductCard from "./ProductCard";
+import CategorySlider from "./CategorySlider"
 
 const allProducts = [...store];
 
-const Store = () => {
+let categories = ["all"];
+allProducts.forEach((prod) => {
+  if(!categories.includes(prod.category)) {
+    categories.push(prod.category);
+  }
+})
 
-    return (
-        <Grid container alignItems="stretch" spacing={4}>
-            {
-                allProducts.map((product) => {
-                    return <ProductCard key={product.id} prod={product} />
-                })
-            }
-        </Grid>
-    )
+const Store = () => {
+  const [selected, setSelected] = useState("all");
+  const [filteredProducts, setFilteredProducts] = useState([...allProducts]);
+
+  const handleSelect = (select) => {
+    setSelected(select);
+  }
+
+  useEffect(()=> {
+    if(selected != "all") {
+      setFilteredProducts(allProducts.filter((prod) => prod.category === selected));
+    }else {
+      setFilteredProducts([...allProducts]);
+    };
+  },[selected])
+
+  return (
+    <>
+      <CategorySlider categories={categories} selected={selected} onSelect={handleSelect} />
+      <Grid container alignItems="stretch" spacing={4}>
+        {
+          filteredProducts.map((product) => {
+            return <ProductCard key={product.id} prod={product} />
+          })
+        }
+      </Grid>
+    </>
+  )
 }
 
 export default Store
