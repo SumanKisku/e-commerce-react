e1b782324e4df98c4c9d2fdd0697029da0602f6b
Suman Kisku <sumankisku1@gmail>
2023-08-17 20:08:46 +0530

    little bit tweaks in payment page

--- src/components/Payment/PaymentButton.jsx
+++ src/components/Payment/PaymentButton.jsx
@@ -11,25 +11,14 @@ const PaymentButton = () => {
 
     const handleClick = () => {
         dispatch({ type: "clearCart" });
-        toast.success("Payment successful", {
-            style: {
-                border: '1px solid #1976d2',
-                padding: '16px',
-                color: '#1976d2',
-            },
-            iconTheme: {
-                primary: 'green',
-                secondary: '#FFFAEE',
-            },
-        });
     }
     return (
         <Button sx={{ textAlign: 'center' }} variant="contained" onClick={handleClick}>
-            <Link to="/paymentsuccess">
-                Procced to Buy
-            </Link>
-        </Button>
-    )
+      <Link to="/paymentsuccess">
+        Procced to Buy
+      </Link>
+    </Button>
+  )
 }
 
 export default PaymentButton
