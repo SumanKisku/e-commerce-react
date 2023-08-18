cb645e69521ac97e105f97e344df26d5bb365c72
Suman Kisku <sumankisku1@gmail>
2023-08-18 12:09:04 +0530

    little changes in about

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,16 +1,16 @@
 
 const AboutPage = () => {
   return (
-    <div className="mx-2.5 mt-24">
-      <div className="p-8 bg-gray-100">
+    <div className="mx-2.5 mt-20">
+      <div className="p-8">
         <h1 className="mb-4 text-2xl font-semibold">About Section for E-Commerce Project</h1>
         <p>Welcome to my E-Commerce project</p>
 
-        <h2 className="mt-6 text-lg font-semibold">My Technology Stack:</h2>
+        <h2 className="mt-6 text-lg font-semibold">Tech Stack I have used:</h2>
         <ul className="pl-6 mt-2 list-disc">
           <li>React</li>
           <li>Material-UI (MUI)</li>
-          <li>Tailwind CSS:</li>
+          <li>Tailwind CSS</li>
         </ul>
 
         <h2 className="mt-6 text-lg font-semibold">Things I have learned while doing this project</h2>
@@ -18,6 +18,7 @@ const AboutPage = () => {
           <li>How to use Context API</li>
           <li>How to use useReducer</li>
           <li>How to use react-router-dom for creating routes</li>
+          <li>Props-Types</li>
         </ul>
       </div>
     </div>
